66cc893b2fcb07e90cb696a4e0c47c27e1bba078
Maxime Masson <maximemasson30@gmail.com>
2024-12-12 23:20:12 -0500

    feat(wip): home page layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,41 @@
 <script>
 	import '@/app.css';
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
+	const date = new Date();
+	const year = date.getFullYear().toString();
+	const month = months[date.getMonth()];
+	const day = date.getDate().toString();
 </script>
 
-<slot />
+<div class="flex flex-col h-svh">
+	<header>
+		<!-- wrapper -->
+		<div class="w-full flex justify-between p-8">
+			<!-- left nav -->
+			<div>Logo Oseadea Confoo</div>
+			<!-- right nav -->
+			<div>Sale gueule de PD</div>
+		</div>
+	</header>
+
+	<slot />
+	<footer>
+		<div class="flex justify-between p-8">
+			<div>{month} {day} {year}</div>
+			<div>Montreal, Canada</div>
+		</div>
+	</footer>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
 
-<a href="/ws">Go to /ws</a>
+<div class="flex-grow flex">
+	<div class="grid grid-[1fr] w-full text-center max-w-xl m-auto justify-center">
+		<h1 class="text-2xl mb-8">AI-Powered Clean Talk:<br />Advanced Profanity Detection Patterns</h1>
+		<div class="mb-8">
+			<p>
+				Advanced profanity detection goes beyond simple keyword searches. This session will cover
+				data collection and training of a profanity detection model, and its integration with live
+				Speech-to-Text AI technologies. We will also look at the challenges around handling live
+				data.
+			</p>
+		</div>
+		<div class="w-full justify-center flex space-x-4">
+			<a href="/ws" class="rounded-sm bg-blue-600 text-white px-4 py-2">New meeting</a>
+			<button class="rounded-sm px-4 py-2 shadow-md">Enter code</button>
+			<button class="rounded-sm px-4 py-2">Join</button>
+		</div>
+	</div>
+</div>
