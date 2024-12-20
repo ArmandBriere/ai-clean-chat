13442e122a03fd305565d75c36aa47271b6edc7b
Armand Briere <armand.123@live.fr>
2024-12-19 21:56:43 -0500

    feat: sherpa hot reload works, write transcription on ws

--- backend/webrtcServer/audioStreaming.go
+++ backend/webrtcServer/audioStreaming.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/pion/webrtc/v4"
 	"github.com/pion/webrtc/v4/pkg/media/oggwriter"
 )
@@ -28,38 +29,8 @@ func getFileAndWriter(writer *oggwriter.OggWriter) (*os.File, *oggwriter.OggWrit
 }
 
 // handleAudioStream handles the audio stream by writing it to file
-func handleAudioStream(track *webrtc.TrackRemote, isStreaming *bool) {
+func handleAudioStream(track *webrtc.TrackRemote, isStreaming *bool, quit chan bool, wsConn *websocket.Conn) {
 
-	// WIP: Use transcribe that is defined in sherpa.go
 	// This take the audio stream for ever
-	Transcribe(track)
-
-	var writer *oggwriter.OggWriter
-	for {
-		// Read RTP packets to flush the buffer
-		rtpPacket, _, err := track.ReadRTP()
-		if err != nil {
-			slog.Error("rtpPacket setup", "Error", err)
-			return
-		}
-		if *isStreaming {
-			if writer == nil {
-				var f *os.File
-				f, writer, err = getFileAndWriter(writer)
-				if err != nil {
-					slog.Error("Error creating file and oggwriter", "Error", err)
-					return
-				}
-				defer func() {
-					writer.Close()
-					f.Close()
-				}()
-			}
-
-			if err := writer.WriteRTP(rtpPacket); err != nil {
-				slog.Error("write RTP", "Error", err)
-				return
-			}
-		}
-	}
+	transcribe(track, isStreaming, quit, wsConn)
 }
--- backend/webrtcServer/sherpa.go
+++ backend/webrtcServer/sherpa.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"strings"
 
+	"github.com/gorilla/websocket"
 	"github.com/hraban/opus"
 	sherpa "github.com/k2-fsa/sherpa-onnx-go/sherpa_onnx"
 	"github.com/pion/webrtc/v4"
@@ -14,19 +15,17 @@ import (
 var stream *sherpa.OnlineStream
 var recognizer *sherpa.OnlineRecognizer
 
-const (
-	sampleRate = 48000
-)
-
+// init initializes the recognizer and stream
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 	config := sherpa.OnlineRecognizerConfig{}
 	config.FeatConfig = sherpa.FeatureConfig{SampleRate: MODEL_SAMPLE_RATE, FeatureDim: 80}
 
-	// Configure the model paths
+	// Zipformer2 configuration
 	// config.ModelConfig.Zipformer2Ctc.Model = "./sherpa-onnx-streaming-zipformer-ctc-small-2024-03-18/ctc-epoch-30-avg-3-chunk-16-left-128.int8.onnx"
 	// config.ModelConfig.Tokens = "./sherpa-onnx-streaming-zipformer-ctc-small-2024-03-18/tokens.txt"
 	// config.CtcFstDecoderConfig.Graph = "./sherpa-onnx-streaming-zipformer-ctc-small-2024-03-18/HLG.fst"
+	// config.ModelConfig.ModelType = "zipformer2"
 
 	config.ModelConfig.NumThreads = 8
 	config.ModelConfig.Debug = 0
@@ -37,7 +36,6 @@ func init() {
 	config.ModelConfig.Transducer.Decoder = "./" + defaultPath + "decoder-epoch-99-avg-1.onnx"
 	config.ModelConfig.Transducer.Joiner = "./" + defaultPath + "joiner-epoch-99-avg-1.onnx"
 	config.ModelConfig.Tokens = "./" + defaultPath + "tokens.txt"
-	// config.ModelConfig.ModelType = "zipformer2"
 
 	slog.Info("Initializing recognizer (may take several seconds)")
 	recognizer = sherpa.NewOnlineRecognizer(&config)
@@ -47,60 +45,78 @@ func init() {
 	slog.Info("Stream created!")
 }
 
-func Transcribe(track *webrtc.TrackRemote) {
+// Transcribe transcribes the audio stream
+func transcribe(track *webrtc.TrackRemote, isStreaming *bool, quit chan bool, wsConn *websocket.Conn) {
 
 	var last_text string
 	segment_idx := 0
 
 	// Create an Opus decoder
-	decoder, err := opus.NewDecoder(48000, 1) // Mono channel
+	decoder, err := opus.NewDecoder(INPUT_SAMPLE_RATE, 1) // Mono channel
 	if err != nil {
 		slog.Error("Failed to create Opus decoder", "error", err)
 	}
 
 	for {
-		rtpPacket, _, err := track.ReadRTP()
-		if err != nil {
-			log.Fatalf("Failed to read RTP packet: %v", err)
-		}
-
-		payload := rtpPacket.Payload
-		if len(payload) == 0 {
-			continue
-		}
-
-		// Decode RTP payload into PCM samples (depends on your audio codec)
-		pcmSamples, err := decodeRTPPayload(decoder, payload)
-		if err != nil {
-			pcmSamples = []int16{0}
-		}
-
-		// Convert PCM samples ([]int16) to []float32
-		samples := pcmToFloat32(pcmSamples)
-
-		// Process samples
-		stream.AcceptWaveform(int(INPUT_SAMPLE_RATE), samples)
-
-		for recognizer.IsReady(stream) {
-			recognizer.Decode(stream)
-		}
-
-		text := recognizer.GetResult(stream).Text
-		if len(text) != 0 && last_text != text {
-			last_text = strings.ToLower(text)
-			fmt.Printf("\r%d: %s", segment_idx, last_text)
-		}
-
-		if recognizer.IsEndpoint(stream) {
-			if len(text) != 0 {
-				segment_idx++
-				fmt.Println()
+		select {
+		case <-quit:
+			slog.Info("Transcription stopped by quit signal")
+			return
+		default:
+			rtpPacket, _, err := track.ReadRTP()
+			if err != nil {
+				slog.Error("Failed to read RTP packet", "packet", err)
 				recognizer.Reset(stream)
+				continue
+			}
+
+			// Skip if user is not streaming
+			if !*isStreaming {
+				continue
+			}
+
+			payload := rtpPacket.Payload
+			if len(payload) == 0 {
+				continue
+			}
+
+			// Decode RTP payload into PCM samples (depends on your audio codec)
+			pcmSamples, err := decodeRTPPayload(decoder, payload)
+			if err != nil {
+				pcmSamples = []int16{0}
+			}
+
+			// Convert PCM samples ([]int16) to []float32
+			samples := PcmToFloat32(pcmSamples)
+
+			// Process samples
+			stream.AcceptWaveform(int(INPUT_SAMPLE_RATE), samples)
+
+			for recognizer.IsReady(stream) {
+				recognizer.Decode(stream)
+			}
+
+			text := recognizer.GetResult(stream).Text
+			if len(text) != 0 && last_text != text {
+				last_text = strings.ToLower(text)
+				// fmt.Printf("\r%d: %s", segment_idx, last_text)
+				slog.Info("Transcription", "text", last_text)
+				wsConn.WriteJSON(WebSocketTranscription{Type: "transcription", Text: last_text})
+				recognizer.Reset(stream) // Hard reset to only get last audio segment
+			}
+
+			if recognizer.IsEndpoint(stream) {
+				if len(text) != 0 {
+					segment_idx++
+					fmt.Println()
+					recognizer.Reset(stream)
+				}
 			}
 		}
 	}
 }
 
+// decodeRTPPayload decodes the RTP payload into PCM samples
 func decodeRTPPayload(decoder *opus.Decoder, payload []byte) ([]int16, error) {
 	// Allocate space for PCM samples
 	// Max Opus frame size is 120ms: 48,000 Hz * 0.06 seconds = 5760 samples
@@ -114,21 +130,4 @@ func decodeRTPPayload(decoder *opus.Decoder, payload []byte) ([]int16, error) {
 
 	// Return the decoded PCM samples
 	return pcm[:n], nil
-}
-
-func pcmToFloat32(pcm []int16) []float32 {
-	floatSamples := make([]float32, len(pcm))
-	for i, sample := range pcm {
-		// Normalize from int16 (-32768 to 32767) to float32 (-1.0 to 1.0)
-		floatSamples[i] = float32(sample) / 32768.0
-	}
-	return floatSamples
-}
-
-func intToFloat32(arr []int) []float32 {
-	result := make([]float32, len(arr))
-	for i, v := range arr {
-		result[i] = float32(v)
-	}
-	return result
 }
