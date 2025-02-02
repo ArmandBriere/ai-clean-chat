0cdf7d0286211d92d76e2d4efe3960e11430d1b1
Maxime Masson <maximemasson30@gmail.com>
2025-02-01 22:01:24 -0500

    feat: glitchy animate video block when remove user join

--- frontend/src/routes/streaming/+page.svelte
+++ frontend/src/routes/streaming/+page.svelte
@@ -10,14 +10,17 @@
   } from '@/lib/constants/types';
 
   let roomID: string = 'example-room'; // Will be replaced with dynamic room ID
-  let userVideo: HTMLVideoElement = $state() as HTMLVideoElement;
-  let otherVideo: HTMLVideoElement = $state() as HTMLVideoElement;
+  let userVideo: HTMLVideoElement;
+  let otherVideo: HTMLVideoElement;
 
   let ws: WebSocket;
   let peerConnection: RTCPeerConnection;
   let localStream: MediaStream;
 
+  let connectedUsers = $state(1);
+
   onMount(() => {
+    console.log(otherVideo?.srcObject);
     // Connect to the signaling server
     ws = new WebSocket(`${PUBLIC_SERVER_WS_URL}/join?roomID=${roomID}&userID=${v4()}`);
 
@@ -95,6 +98,7 @@
       console.log('Received remote track:', event.streams);
       if (event.streams.length > 0) {
         otherVideo.srcObject = event.streams[0];
+        connectedUsers = 2;
       }
     };
 
@@ -148,53 +152,41 @@
 
 <main class="flex min-h-screen flex-col items-center justify-center bg-[rgb(25,25,25)] p-24">
   <div class="relative aspect-video w-full max-w-5xl overflow-hidden rounded-lg bg-gray-900">
-    <div class="relative aspect-video w-full max-w-5xl overflow-hidden rounded-lg bg-gray-900">
-      <div class={`h-full w-full opacity-100 transition-opacity duration-500`}>
-        <div class="h-full w-full overflow-hidden rounded-lg border-2 border-white">
-          <video
-            class="h-full w-full object-cover"
-            autoPlay
-            playsInline
-            muted
-            bind:this={otherVideo}
-          >
-            Your browser does not support the video tag.
-          </video>
-        </div>
+    <div
+      class={`h-full w-full ${connectedUsers === 2 ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500`}
+    >
+      <div class="h-full w-full overflow-hidden rounded-lg border-2 border-white">
+        <video class="h-full w-full object-cover" autoPlay playsInline muted bind:this={otherVideo}>
+          Your browser does not support the video tag.
+        </video>
       </div>
-      <div
-        class={`absolute bottom-4 right-4 z-30 aspect-video w-1/4 transition-all duration-500 ease-in-out`}
-      >
-        <div
-          class={`h-full w-full overflow-hidden rounded-lg ${
-            otherVideo?.srcObject ? 'border-2 border-white' : 'border-2 border-white shadow-lg'
-          }`}
-        >
-          <video
-            class="h-full w-full object-cover"
-            autoPlay
-            muted
-            playsInline
-            bind:this={userVideo}
-          >
-            Your browser does not support the video tag.
-          </video>
-        </div>
-      </div>
-      <!-- <div class="absolute bottom-4 left-1/2 z-20 flex -translate-x-1/2 transform space-x-4"> -->
-      <!--   <button -->
-      <!--     onClick={() => setIsMicOn(!isMicOn)} -->
-      <!--     class={`rounded-full p-3 ${isMicOn ? 'bg-gray-200 dark:text-black' : 'bg-red-500'}`} -->
-      <!--   > -->
-      <!--     {isMicOn ? <Mic /> : <MicOff />} -->
-      <!--   </button> -->
-      <!--   <button -->
-      <!--     onClick={() => setIsVideoOn(!isVideoOn)} -->
-      <!--     class={`rounded-full p-3 ${isVideoOn ? 'bg-gray-200 dark:text-black' : 'bg-red-500'}`} -->
-      <!--   > -->
-      <!--     {isVideoOn ? <Video /> : <VideoOff />} -->
-      <!--   </button> -->
-      <!-- </div> -->
     </div>
+    <div
+      class={`absolute ${connectedUsers === 2 ? 'bottom-4 right-4 z-30 aspect-video w-1/4' : 'inset-0 z-10'} transition-all duration-500 ease-in-out`}
+    >
+      <div
+        class={`h-full w-full overflow-hidden rounded-lg ${
+          connectedUsers === 2 ? 'border-2 border-white' : 'border-2 border-white shadow-lg'
+        }`}
+      >
+        <video class="h-full w-full object-cover" autoPlay muted playsInline bind:this={userVideo}>
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+    <!-- <div class="absolute bottom-4 left-1/2 z-20 flex -translate-x-1/2 transform space-x-4"> -->
+    <!--   <button -->
+    <!--     onClick={() => setIsMicOn(!isMicOn)} -->
+    <!--     class={`rounded-full p-3 ${isMicOn ? 'bg-gray-200 dark:text-black' : 'bg-red-500'}`} -->
+    <!--   > -->
+    <!--     {isMicOn ? <Mic /> : <MicOff />} -->
+    <!--   </button> -->
+    <!--   <button -->
+    <!--     onClick={() => setIsVideoOn(!isVideoOn)} -->
+    <!--     class={`rounded-full p-3 ${isVideoOn ? 'bg-gray-200 dark:text-black' : 'bg-red-500'}`} -->
+    <!--   > -->
+    <!--     {isVideoOn ? <Video /> : <VideoOff />} -->
+    <!--   </button> -->
+    <!-- </div> -->
   </div>
 </main>
