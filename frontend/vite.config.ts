3c8a36c2f2653774c0006a54bc50145d8f0762a1
Maxime Masson <maximemasson30@gmail.com>
2025-02-13 15:15:06 -0500

    chores: update dps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
-
   server: {
     allowedHosts: ['ai-clean-chat.home.site']
-  },
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}']
   }
+
+  // test: {
+  //   include: ['src/**/*.{test,spec}.{js,ts}']
+  // }
 });
