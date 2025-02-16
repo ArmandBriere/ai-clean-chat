dfe35e809815e1fd09d750dfa7045c05571b50e8
Armand Briere <armand.123@live.fr>
2025-02-16 10:41:52 -0500

    feat: move header to a component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
+  import Header from '@/lib/components/Header.svelte';
+
   import { goto } from '$app/navigation';
   import { PUBLIC_SERVER_URL } from '$env/static/public';
-  import Image from '@/lib/components/Image.svelte';
-  import logo1 from '$lib/assets/logo/osedea-logo.png';
-  import logo2 from '$lib/assets/logo/X.png';
-  import logo3 from '$lib/assets/logo/confoo-logo.png';
-  import avatar from '$lib/assets/avatar.jpeg';
+
   const months = [
     'January',
     'February',
@@ -64,27 +62,7 @@
 </script>
 
 <div class="flex h-svh flex-col">
-  <header>
-    <!-- wrapper -->
-    <div class="flex w-full justify-between p-8">
-      <!-- left nav -->
-      <a href="/" class="flex items-center">
-        <div class="flex items-center space-x-4">
-          <Image src={logo1} alt="logo" class="w-full object-contain" />
-          <Image src={logo2} alt="logo" class="w-full object-contain" />
-          <Image src={logo3} alt="logo" class="w-full object-contain" />
-        </div>
-      </a>
-      <!-- right nav -->
-      <div>
-        <div class="w-14 overflow-hidden rounded-full">
-          <a href="/health">
-            <Image src={avatar} alt="avatar" class="w-full object-contain" />
-          </a>
-        </div>
-      </div>
-    </div>
-  </header>
+  <Header />
 
   <div class="flex flex-grow">
     <div class="grid-[1fr] m-auto grid w-full max-w-xl justify-center text-center">
